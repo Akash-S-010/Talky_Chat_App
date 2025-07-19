0e9798e5ffe9b13ec1da724822494428ad5bf7a3
Akash S <akashspalloor@gmail.com>
2025-07-19 13:23:52 +0530

    added new pages and designed landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import ChattingPage from './pages/ChattingPage'
 
 const App = () => {
   return (
     <div className='bg-[url("/src/assets/wallpaper.jpg")] bg-cover h-screen'>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChattingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
